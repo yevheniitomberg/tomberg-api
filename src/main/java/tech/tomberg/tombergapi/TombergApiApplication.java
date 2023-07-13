52d438c0a51f19515607b0432c03d47e0b87e75b
Yevhenii Tomberg <yevhenii@tomberg.com>
2023-07-14 00:46:14 +0300

    Added cron for removing contacted person list

--- src/main/java/tech/tomberg/tombergapi/TombergApiApplication.java
+++ src/main/java/tech/tomberg/tombergapi/TombergApiApplication.java
@@ -2,7 +2,9 @@ package tech.tomberg.tombergapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class TombergApiApplication {
 
--- src/main/java/tech/tomberg/tombergapi/utils/ScheduledTasksUtil.java
+++ src/main/java/tech/tomberg/tombergapi/utils/ScheduledTasksUtil.java
@@ -1,4 +1,16 @@
 package tech.tomberg.tombergapi.utils;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import tech.tomberg.tombergapi.repository.ContactedPersonRepository;
+
+@Component
+@RequiredArgsConstructor
 public class ScheduledTasksUtil {
+    private final ContactedPersonRepository contactedPersonRepository;
+    @Scheduled(cron = "@midnight")
+    public void cleanContactedPersonList() {
+        contactedPersonRepository.deleteAll();
+    }
 }
